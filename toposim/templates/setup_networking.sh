9a524ec4d2f36f8f5d81c56bfb5447bfd9a596dc
Aneesh Durg <aneeshdurg17@gmail.com>
2024-07-16 08:48:59 -0500

    Avoid collision between subnets that share a prefix (172.1 and 172.10)

--- toposim/templates/setup_networking.sh
+++ toposim/templates/setup_networking.sh
@@ -11,7 +11,7 @@ forward() {
 }
 
 get_iface_for_subnet() {
-  run_in_ns $1 ip addr | grep "inet $2" | awk '{ print $NF }'
+  run_in_ns $1 ip addr | grep "inet $2\." | awk '{ print $NF }'
 }
 
 # for every port, set up packet forwarding

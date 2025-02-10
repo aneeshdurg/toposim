663e53dd8349448ce919bf316ac4e6e4fb3afbb2
Aneesh Durg <aneeshdurg17@gmail.com>
2025-02-10 16:56:05 -0600

    Explicitly model OCS states

--- heatmap/simulate_reconfig.py
+++ heatmap/simulate_reconfig.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import multiprocessing as mp
 import random
+from dataclasses import dataclass, field
 
 import numpy as np
 
@@ -29,39 +30,131 @@ N = max(
     ]
 )
 
-paths_ = {
-    (0, 1): 4,
-    (0, 2): 5,
-    (0, 3): 3,
-    (0, 4): 4,
-    (0, 5): 5,
-    (1, 0): 3,
-    (1, 2): 5,
-    (1, 3): 3,
-    (1, 4): 4,
-    (1, 5): 5,
-    (2, 0): 3,
-    (2, 1): 5,
-    (2, 3): 3,
-    (2, 4): 4,
-    (2, 5): 5,
-    (3, 0): 0,
-    (3, 1): 1,
-    (3, 2): 2,
-    (3, 4): 1,
-    (3, 5): 2,
-    (4, 0): 0,
-    (4, 1): 1,
-    (4, 2): 2,
-    (4, 3): 0,
-    (4, 5): 2,
-    (5, 0): 0,
-    (5, 1): 1,
-    (5, 2): 2,
-    (5, 3): 1,
-    (5, 4): 1,
-}
-racks = [[0, 3], [1, 4], [2, 5]]
+
+@dataclass
+class Node:
+    id_: int
+
+    def __hash__(self):
+        return hash(self.id_)
+
+
+@dataclass
+class Rack:
+    "Two nodes in the same rack (immutable)"
+    nodes: tuple[Node, Node]
+
+
+@dataclass
+class OCS:
+    rack: tuple[Rack, Rack]
+    # If state is false:
+    #   rack[0][0] -> rack[1][1]/rack[0][1] -> rack[0][0]
+    # Else:
+    #   rack[0][0] -> rack[1][0]/rack[0][1] -> rack[0][1]
+    state: bool = False
+
+    def adjacent(self, n: Node) -> Node:
+        "Determine what node `n` is connected to via the OCS link"
+        if n in self.rack[0].nodes:
+            i = self.rack[0].nodes.index(n)
+            if self.state:
+                return self.rack[1].nodes[i]
+            return self.rack[1].nodes[(i + 1) % 2]
+
+        assert n in self.rack[1].nodes
+        i = self.rack[1].nodes.index(n)
+        if self.state:
+            return self.rack[0].nodes[i]
+        return self.rack[0].nodes[(i + 1) % 2]
+
+
+@dataclass
+class Topology:
+    ocss: list[OCS]
+    adjacency: dict[Node, set[Node]] = field(default_factory=lambda: {})
+    paths: dict[tuple[int, int], int] = field(default_factory=lambda: {})
+
+    def __post_init__(self):
+        self.rebuild_all()
+
+    def rebuild_all(self):
+        self.rebuild_adjacency()
+        self.rebuild_paths()
+
+    def rebuild_adjacency(self):
+        adj = {}
+        for ocs in self.ocss:
+            ocs_nodes = []
+            for r in ocs.rack:
+                if r.nodes[0] not in adj:
+                    adj[r.nodes[0]] = set()
+                if r.nodes[1] not in adj:
+                    adj[r.nodes[1]] = set()
+
+                adj[r.nodes[0]].add(r.nodes[1])
+                adj[r.nodes[1]].add(r.nodes[0])
+                ocs_nodes.append(r.nodes[0])
+                ocs_nodes.append(r.nodes[1])
+            for n in ocs_nodes:
+                adj[n].add(ocs.adjacent(n))
+
+        self.adjacency = adj
+
+    def rebuild_paths(self):
+        paths: dict[tuple[Node, Node], tuple[list[Node], float]] = {}
+
+        # Floyd-Warshall all shortest paths
+        for src in self.adjacency:
+            for dst in self.adjacency:
+                if src == dst:
+                    continue
+                dist = float("inf")
+                if dst in self.adjacency[src]:
+                    dist = 1
+                paths[(src, dst)] = ([dst], dist)
+
+        for n in self.adjacency:
+            for src in self.adjacency:
+                for dst in self.adjacency:
+                    if src.id_ == dst.id_:
+                        continue
+
+                    if n == src or n == dst:
+                        continue
+                    curr_cost = paths[(src, dst)][1]
+                    new_cost = paths[(src, n)][1] + paths[(n, dst)][1]
+                    if new_cost < curr_cost:
+                        paths[(src, dst)] = (
+                            paths[(src, n)][0] + paths[(n, dst)][0],
+                            new_cost,
+                        )
+        # Convert Node to int for compatibility with existing code
+        self.paths = {}
+        for k in paths:
+            self.paths[(k[0].id_, k[1].id_)] = paths[k][0][0].id_
+
+    def set_ocs_states(self, states: list[bool]):
+        assert len(states) == len(self.ocss)
+        for s, ocs in zip(states, self.ocss):
+            ocs.state = s
+        self.rebuild_all()
+
+
+# Model slimfly as Node/Rack/OCSs
+n0 = Node(0)
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+
+r0 = Rack((n0, n3))
+r1 = Rack((n1, n4))
+r2 = Rack((n2, n5))
+
+ocs = [OCS((r0, r1)), OCS((r0, r2)), OCS((r1, r2))]
+tp = Topology(ocs)
 
 
 def process_ts(ts):
@@ -73,28 +166,15 @@ def process_ts(ts):
                 matrix[i][j] = m[i][j]
 
     def permute(p: int):
-        paths = {k: v for k, v in paths_.items()}
+        # Get `p` as a bitstring showing whether each OCS state should be
+        # toggled or not.
         b = bin(p)[2:]
-        b = (len(racks) - len(b)) * "0" + b
-        for i in range(len(racks)):
-            if b[i] == "0":
-                continue
-
-            def swap(x):
-                if x == racks[i][0]:
-                    x = racks[i][1]
-                elif x == racks[i][1]:
-                    x = racks[i][0]
-                return x
-
-            new_paths = {}
-            # swap racks[i][0] and racks[i][1]
-            for k, v in paths.items():
-                start = swap(k[0])
-                end = swap(k[1])
-                v = swap(v)
-                new_paths[(start, end)] = v
-            paths = new_paths
+        b = (len(tp.ocss) - len(b)) * "0" + b
+        state = [c == "1" for c in b]
+        # Toggle all OCS states for this permutation and get the new next-hops
+        # for each path
+        tp.set_ocs_states(state)
+        paths = tp.paths
 
         combined_matrix = np.zeros((num_groups, num_groups), dtype=int)
         for src in range(6):
@@ -115,7 +195,7 @@ def process_ts(ts):
         return combined_matrix
 
     ms = []
-    for i in range(2 ** len(racks)):
+    for i in range(2 ** len(tp.ocss)):
         m = permute(i)
         ms.append(m)
     return [sum(sum(m)) for m in ms]
@@ -174,17 +254,21 @@ show_report(best_case, best_path)
 print("\nSingle OCS change at a time reconfig strategy (oracle):")
 curr_config = [int(np.argmin(ts_costs[0]))]
 curr_cost = min(ts_costs[0])
+
+
 def hamming_distance(a, b):
     bs_a = bin(a)[2:]
     bs_b = bin(b)[2:]
     max_len = max(len(bs_a), len(bs_b))
-    bs_a = '0' * (max_len - len(bs_a)) + bs_a
-    bs_b = '0' * (max_len - len(bs_b)) + bs_b
+    bs_a = "0" * (max_len - len(bs_a)) + bs_a
+    bs_b = "0" * (max_len - len(bs_b)) + bs_b
     d = 0
-    for (ca, cb) in zip(bs_a, bs_b):
+    for ca, cb in zip(bs_a, bs_b):
         if ca != cb:
             d += 1
     return d
+
+
 for i in range(1, len(ts_costs)):
     # Evaluate every config that differs by at most 1 bit from the current config
     min_cost = -1
@@ -227,7 +311,7 @@ for i in range(len(ts_costs)):
     random_path.append(config)
 show_report(random_cost, random_path)
 
-print('paths', 1 - sum(np.array(best_path) == np.array(one_ocs_path)) / len(best_path))
+print("paths", 1 - sum(np.array(best_path) == np.array(one_ocs_path)) / len(best_path))
 total_diff = 0
 for i in range(len(best_path)):
     if best_path[i] != one_ocs_path[i]:
